9832e5bf7c7d8636f82ddca35974543e6c09a6e7
vladimirtishenko <vladimirtishenko1@gmail.com>
2016-01-11 16:29:44 +0200

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(app){
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+app.get("/", require("./front").get);
 
-module.exports = router;
+
+}
