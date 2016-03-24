580b7cec208795df284e75dc2ef59c1690cd8620
vladimir.tischenko <vladimir.tischenko@oracle.com>
2016-03-24 11:36:46 +0200

    mails

--- routes/mail.js
+++ routes/mail.js
@@ -1,35 +1,34 @@
 var nodemailer = require('nodemailer');
+var smtpTransport = require("nodemailer-smtp-transport");
 var config = require('../config/');
+var smtpTransport = nodemailer.createTransport(smtpTransport({
+    host: config.get('mail:host'), 
+    secureConnection: true, 
+    port: config.get('mail:port'), 
+    auth: {
+        user: config.get('mail:username'),
+        pass: config.get('mail:pass')
+    }
+}));
 
 module.exports.post = function(req, res, next) {
 
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-        host: config.get('mail:host'), 
-        secureConnection: true, 
-        port: config.get('mail:port'), 
-        auth: {
-            user: config.get('mail:username'),
-            pass: config.get('mail:pass')
-        }
-    });
-
-    var mail = {
-        from: config.get('mail:username'),
-        to: config.get('mail:username'),
-        subject: "Send Email Using Node.js",
-        text: "Node.js New world for me",
-        html: "<b>Node.js New world for me</b>"
+   var mailOptions = {
+        from : config.get('mail:username'),
+        to : config.get('mail:username'),
+        subject : "Your Subject",
+        text : "Your Text",
+        html : "HTML GENERATED"
     }
-
-    smtpTransport.sendMail(mail, function(error, response){
+    smtpTransport.sendMail(mailOptions, function(error, response){
         if(error){
             console.log(error);
+            res.json("error");
         }else{
+            console.log(response.response.toString());
             console.log("Message sent: " + response.message);
+            res.json("sent");
         }
-
-        smtpTransport.close();
     });
-
 
 }
