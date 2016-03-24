0a1c3fa5cc5966685f082698db24c3381027efe1
vladimir.tischenko <vladimir.tischenko@oracle.com>
2016-03-24 13:05:56 +0200

    add mail

--- routes/mail.js
+++ routes/mail.js
@@ -5,20 +5,21 @@ var responseResult = require('../middleware/responseResult').responseResult;
 
 module.exports.post = function(req, res, next) {
 
-   var transporter = nodemailer.createTransport(smtpTransport({
-  service: 'Gmail',
-  auth: { user: 'vladimirtishenko1@gmail.com',
-        pass: 'MEDS8placebo' }
-  }));
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'Gmail',
+        auth: {
+            user: '4side.developer@gmail.com',
+            pass: '4side4side'
+        }
+    }));
 
-  transporter.sendMail({
-    from: 'support@4side.in.ua',
-    to: "vladimirtishenko1@gmail.com",
-    subject: 'Test sujet',
-    text: "test text",
-    html: "<b>Test text</b>"
-  }, function (error, response) {
+    transporter.sendMail({
+        from: 'support@4side.in.ua',
+        to: "4side.developer@gmail.com",
+        subject: 'New Message from 4side',
+        text: "Name client: "+req.body.name+"\r\n Number client: "+req.body.number+"\r\n Email client: "+req.body.email+"\r\n Company client: "+req.body.company+"\r\n Description of Project: "+req.body.description
+    }, function(error, response) {
         responseResult(error, res);
-  });
+    });
 
 }
