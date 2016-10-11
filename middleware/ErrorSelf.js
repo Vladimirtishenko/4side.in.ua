44d206425cc32a7176573c6cbb3de140820db7ac
vladimir.tischenko <vladimir.tischenko@oracle.com>
2016-10-11 12:23:24 +0300

    add right 404

--- middleware/ErrorSelf.js
+++ middleware/ErrorSelf.js
@@ -1,7 +1,18 @@
+var Contact = require('../model/contact').Contact;
+
 function ErrorSelf(res, err, next){
 	if(err){
 		if(typeof err == 'string'){err = {}, err.status = '404'}
-		res.render('err', {err: err});
+
+		Contact.find({},
+            function(error, result) {
+                if (error) {
+		            return ErrorSelf(res, error, next);
+		        }
+
+                res.render('err', {err: err, data: result});
+            });
+
 	}
 }
 
